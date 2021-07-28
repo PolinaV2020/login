4a4eb328b945ffd571e705422f7899427c6b8c72
polinav2020 <polinapiksi2019@mail.ru>
2021-07-29 00:26:41 +0300

    feat: add classwork task

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import UserList from "./UserList.js"
 
 
 ReactDOM.render(
   <React.StrictMode>
+  <UserList />
   </React.StrictMode>,
   document.getElementById('root')
 );
